8e94db338ec13efaca05f1d5bd72602a836419e0
leuthold <aliceleuthold@gmail.com>
2019-07-19 00:15:05 +0200

    removed heroku host

--- moviesdb/moviesdb/settings.py
+++ moviesdb/moviesdb/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = CSRF_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'moviesdb-api.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
